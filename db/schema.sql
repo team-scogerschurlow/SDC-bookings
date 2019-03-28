800b5364cddd1f2eb37ae0ce6b8aa8f0e0e776d4
Rachael Otto <rachaelotto@Rachaels-MacBook-Pro.local>
2019-03-27 19:00:46 -0700

    The schema for the MySQL database is now free of any syntactical errors, and will correctly create the database when 'mysql -u < <path/to/schema.sql>' is run.

--- db/schema.sql
+++ db/schema.sql
@@ -2,7 +2,7 @@ CREATE DATABASE IF NOT EXISTS bookings;
 
 USE bookings;
 
-CREATE TABLE rental_price_info IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS rental_price_info (
     id  INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     guest_limit INT NOT NULL DEFAULT 2,
     service_fee INT NOT NULL DEFAULT 10,
@@ -10,13 +10,13 @@ CREATE TABLE rental_price_info IF NOT EXISTS (
     rating INT NOT NULL DEFAULT 3
 );
 
-CREATE TABLE rental_availability IF NOT EXISTS (
-    id  INT NOT NULL AUTO_INCREMENT PRIMARY KEY
+CREATE TABLE rental_availability (
+    id  INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     date DATE NOT NULL,
     price INT NOT NULL DEFAULT 50,
     available INT NOT NULL DEFAULT 0,
     views INT NOT NULL DEFAULT 0,
     rental_id INT,
-    FOREIGN KEY rental_id REFERENCES rental_price_info(id) ON DELETE CASCADE
+    FOREIGN KEY (rental_id) REFERENCES rental_price_info(id) ON DELETE CASCADE
 );
 
