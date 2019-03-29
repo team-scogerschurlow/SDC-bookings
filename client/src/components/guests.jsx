a2e172e58fb070b855d88d82db28ef8a5ad84a55
Rachael Otto <rachaelotto@Rachaels-MacBook-Pro.local>
2019-03-29 15:56:20 -0700

    Started working on guest child component. Switching to new branch to better address

---
+++ client/src/components/guests.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+class Guests extends React.Component {
+    constructor (props) {
+        super(props);
+        
+    }
+
+
+
+    render () {
+        return (
+            <div>
+                <p>Guests</p>
+            </div>
+
+        )
+    }
+
+}
+
+export default Guests;
