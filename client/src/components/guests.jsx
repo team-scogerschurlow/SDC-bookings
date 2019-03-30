c0272d01c374d04ab260e6595d38fbf3bb534faa
Rachael Otto <rachaelotto@Rachaels-MacBook-Pro.local>
2019-03-29 17:26:45 -0700

    Now have functional dropdown menu on Guest component. Also moved the resources directory to be in the public directory in order to ensure that the resources coule be properly utilized. Next steps: Add buttons to each of the options in the dropdown menu to increase or decrease the number.

--- client/src/components/guests.jsx
+++ client/src/components/guests.jsx
@@ -4,18 +4,57 @@ import React from 'react';
 class Guests extends React.Component {
     constructor (props) {
         super(props);
+
+        this.state = {
+            showDropdown: false
+        }
+
+        this.displayDropdown = this.displayDropdown.bind(this);
+        this.hideDropdown = this.hideDropdown.bind(this);
         
     }
 
+    displayDropdown (e) {
+        e.preventDefault();
+        this.setState({
+            showDropdown: true
+        })
+    }
+
+    hideDropdown (e) {
+        e.preventDefault();
+        this.setState({
+            showDropdown: false
+        });
+    }
 
 
     render () {
         return (
             <div>
-                <p>Guests</p>
+                <button onClick={this.displayDropdown}>Guests</button>
+
+                {
+                    this.state.showDropdown 
+                    ? (
+                        <div>
+                        <ul>
+                            <li>Guests</li>
+                            <li>Children</li>
+                            <li>Infants</li>
+                        </ul>
+                        <button onClick={this.hideDropdown}>Close</button>
+                        </div>
+
+                    ) : (
+                        null
+                    )
+
+                    
+                }
             </div>
 
-        )
+        );
     }
 
 }
