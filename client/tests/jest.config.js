a76d521ebe8883f9a6dca5a3248733cf07905b2e
Rachael Otto <rachaelotto@Rachaels-MacBook-Pro.local>
2019-04-08 10:11:04 -0700

    Compiling is currently working with all package versions. About to update all babel-packages to more recent version. Committing as a save point before I break everything

--- client/tests/jest.config.js
+++ client/tests/jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
     collectCoverageFrom: ['./client/src/**/*.{js,jsx}'],
     coverageDirectory: 'coverage',
     moduleFileExtensions: ['js', 'json', 'jsx'],
-    setupFiles: ['<rootDir>/enzyme.config.js'],
+    setupFilesAfterEnv: ['<rootDir>/client/tests/enzyme.config.js'],
     //testMatch: ['**/__tests__/**/*.js?(x)', '**/?(*.)+(spec|test).js?(x)'],
     //testPathIgnorePatterns: ['\\\\node_modules\\\\'],
     transformIgnorePatterns: ['<rootDir>/node_modules/'],
